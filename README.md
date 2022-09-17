f43d9db577f62ca0177db0f103a27704488c779d
Florian <trayhd4415@gmail.com>
2022-09-17 20:31:20 +0200

    updates

--- README.md
+++ README.md
@@ -34,6 +34,45 @@ res = session.get(
 )
 ```
 
+Example 1 - Custom:
+```python
+import tls_client
+
+# You can find more details about the arguments in `session.py` e.g. what 1, 2, 3, 4 etc. represents in h2_settings
+session = tls_client.Session(
+    ja3_string="771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513,29-23-24,0",
+    h2_settings={
+      1: 65536,
+      3: 1000,
+      4: 6291456,
+      6: 262144
+    },
+    h2_settings_order=[
+        1,
+        3,
+        4,
+        6
+    ],
+    pseudo_header_order=[
+        ":method",
+        ":authority",
+        ":scheme",
+        ":path"
+    ],
+    connection_flow=15663105
+)
+
+res = session.post(
+    "https://www.example.com/",
+    headers={
+        "key1": "value1",
+    },
+    json={
+        "key1": "key2"
+    }
+)
+```
+
 # Acknowledgements
 Big shout out to [Bogdanfinn](https://github.com/bogdanfinn) for open sourcing his [tls-client](https://github.com/bogdanfinn/tls-client) in Golang.
 Also to [requests](https://github.com/psf/requests), as most of the cookie handling is copied from it. :'D
--- examples/example2 - custom.py
+++ examples/example2 - custom.py
@@ -1,21 +1,35 @@
 import tls_client
 
-# You can also use the following as `client_identifier`:
-# Chrome --> chrome_103, chrome_104, chrome_105
-# Firefox --> firefox_102, firefox_104
-# Opera --> opera_89, opera_90
-# Safari --> safari_15_3, safari_15_6_1, safari_16_0
-# iOS --> safari_ios_15_5, safari_ios_15_6, safari_ios_16_0
-# iPadOS --> safari_ios_15_6
-
+# You can find more details about the arguments in `session.py` e.g. what 1, 2, 3, 4 etc. represents in h2_settings
 session = tls_client.Session(
-    client_identifier="chrome_105"
+    ja3_string="771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513,29-23-24,0",
+    h2_settings={
+      1: 65536,
+      3: 1000,
+      4: 6291456,
+      6: 262144
+    },
+    h2_settings_order=[
+        1,
+        3,
+        4,
+        6
+    ],
+    pseudo_header_order=[
+        ":method",
+        ":authority",
+        ":scheme",
+        ":path"
+    ],
+    connection_flow=15663105
 )
 
-res = session.get(
+res = session.post(
     "https://www.example.com/",
     headers={
         "key1": "value1",
     },
-    proxy="http://user:password@host:port"
+    json={
+        "key1": "key2"
+    }
 )
