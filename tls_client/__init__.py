42ed2ff352935cc65547d99fed4d535b0b754b56
Florian <trayhd4415@gmail.com>
2022-09-17 19:11:07 +0200

    first

--- tls_client/__init__.py
+++ tls_client/__init__.py
@@ -7,6 +7,7 @@
 # Disclaimer:
 # Big shout out to Bogdanfinn for open sourcing his tls-client in Golang.
 # Also to requests, as most of the cookie handling is copied from it. :'D
+# I wanted to keep the syntax as similar as possible to requests, as most people use it and are familiar with it!
 # Links:
 # tls-client: https://github.com/bogdanfinn/tls-client
 # requests: https://github.com/psf/requests
