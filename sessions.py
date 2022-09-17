0399b5be3f9ec5c9da3ef83fcd176bbde2ec6d8a
Florian <trayhd4415@gmail.com>
2022-09-17 11:04:37 +0200

    first

--- sessions.py
+++ sessions.py
@@ -1,10 +1,21 @@
 from .structures import CaseInsensitiveDict
 from .__version__ import __version__
 
+from typing import Optional
+
 class Session:
 
-    def __init__(self):
-
+    def __init__(
+        self,
+        client_identifier: Optional[str] = None,
+        ja3_string: Optional[str] = None,
+        h2_settings: Optional[dict[int, int]] = None,
+        h2_settings_order: Optional[list[int]] = None,
+        pseudo_header_order: Optional[list[str]] = None,
+        connection_flow: Optional[int] = None,
+        priority_frames: Optional[str] = None,
+        header_order: Optional[list[str]] = None,
+    ) -> None:
         # --- Standard Settings ----------------------------------------------------------------------------------------
 
         # Case-insensitive dictionary of headers, send on each request
@@ -33,3 +44,11 @@ class Session:
 
         # --- Advanced Settings ----------------------------------------------------------------------------------------
 
+        #client_identifier
+        #ja3_string
+        #h2_settings
+        #h2_settings_order
+        #pseudo_header_order
+        #connection_flow
+        #priority_frames
+        #header_order
