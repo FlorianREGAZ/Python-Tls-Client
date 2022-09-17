34a1258780cbec9d13b6622504b7bf57ee8aad36
Florian <trayhd4415@gmail.com>
2022-09-17 10:42:28 +0200

    first

---
+++ sessions.py
@@ -0,0 +1,35 @@
+from .structures import CaseInsensitiveDict
+from .__version__ import __version__
+
+class Session:
+
+    def __init__(self):
+
+        # --- Standard Settings ----------------------------------------------------------------------------------------
+
+        # Case-insensitive dictionary of headers, send on each request
+        self.headers = CaseInsensitiveDict(
+            {
+                "User-Agent": f"tls-client/{__version__}",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept": "*/*",
+                "Connection": "keep-alive",
+            }
+        )
+
+        # Example:
+        # {
+        #     "http": "http://user:pass@ip:port",
+        #     "https": "http://user:pass@ip:port"
+        # }
+        self.proxies = {}
+
+        # Dictionary of querystring data to attach to each request. The dictionary values may be lists for representing
+        # multivalued query parameters.
+        self.params = {}
+
+        # CookieJar containing all currently outstanding cookies set on this session
+        self.cookies = cookiejar_from_dict({})
+
+        # --- Advanced Settings ----------------------------------------------------------------------------------------
+
