269e2286e3d9af99b690481727f84a9e54801f27
fancy45daddy <124528204+fancy45daddy@users.noreply.github.com>
2024-03-16 04:14:11 -0700

    Update sessions.py

--- tls_client/sessions.py
+++ tls_client/sessions.py
@@ -310,7 +310,7 @@ class Session:
         headers: Optional[dict] = None,  # Optional[dict[str, str]]
         cookies: Optional[dict] = None,  # Optional[dict[str, str]]
         json: Optional[dict] = None,  # Optional[dict]
-        allow_redirects: Optional[bool] = False,
+        allow_redirects: Optional[bool] = True,
         insecure_skip_verify: Optional[bool] = False,
         timeout_seconds: Optional[int] = None,
         proxy: Optional[dict] = None  # Optional[dict[str, str]]
